4d0c1afbe726d92d1e3a47f9f820e6170f31f5b1
WeetHet <stas.ale66@gmail.com>
2024-11-04 17:48:40 +0200

    Add matplotlib as a dependency

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,7 +1,7 @@
 { lib, workspace, ... }:
 let
   overlay' = workspace.mkPyprojectOverlay {
-    sourcePreference = "sdist";
+    sourcePreference = "wheel";
   };
   # - https://adisbladis.github.io/uv2nix/FAQ.html
   # - https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md
--- src/janhke_emde/__init__.py
+++ src/janhke_emde/__init__.py
@@ -1,2 +1,4 @@
+import matplotlib.pyplot as plt
+
 def hello() -> None:
     print("Hello from py-dev!")
