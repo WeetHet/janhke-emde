0d6863e80b908812cb26b9ebb1a225fa7c7dc48c
WeetHet <stas.ale66@gmail.com>
2025-01-12 17:31:40 +0200

    Use treefmt for formatting

--- flake.nix
+++ flake.nix
@@ -1,30 +1,55 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flakelight = {
-      url = "github:nix-community/flakelight";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs =
-    { flakelight, pyproject-nix, ... }:
-    flakelight ./. (
-      { lib, ... }:
-      {
-        systems = lib.systems.flakeExposed;
-        package = { callPackage }: callPackage ./defs/package.nix { inherit pyproject-nix; };
-        devShell.packages = pkgs: [
-          (import ./defs/pythonEnv.nix {
-            inherit (pkgs) python3;
-            inherit pyproject-nix;
-          })
-          pkgs.ruff
-        ];
-        formatter = pkgs: pkgs.nixfmt-rfc-style;
-      }
-    );
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages.default = pkgs.callPackage ./defs/package.nix {
+            inherit (inputs) pyproject-nix;
+          };
+
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              (import ./defs/pythonEnv.nix {
+                inherit (pkgs) python3;
+                inherit (inputs) pyproject-nix;
+              })
+              pkgs.ruff
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.ruff = {
+              check = true;
+              format = true;
+            };
+            programs.nixfmt.enable = true;
+            programs.shfmt.enable = true;
+            programs.taplo.enable = true;
+
+            settings.formatter = {
+              ruff-check.priority = 1;
+              ruff-format.priority = 2;
+            };
+          };
+        };
+    };
 }
